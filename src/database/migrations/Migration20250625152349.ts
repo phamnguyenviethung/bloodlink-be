16146e2a69d809e86f9650cb554faebac6eae5dd
Viet Hung <viethung01234@gmail.com>
2025-06-25 22:24:05 +0700

    add migration

---
+++ src/database/migrations/Migration20250625152349.ts
@@ -0,0 +1,132 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250625152349 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`alter table "account" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "account" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "account" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "account" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "admin" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "admin" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "admin" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "admin" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "campaign" add column "blood_collection_date" timestamptz null, add column "metadata" jsonb null;`);
+    this.addSql(`alter table "campaign" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "campaign" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "campaign" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "campaign" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "customer" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "customer" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "customer" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "customer" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "campaign_donation" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "campaign_donation" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "blood_unit" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "blood_unit" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "emergency_request" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "emergency_request" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "hospital" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "hospital" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "hospital" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "hospital" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "staff" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "staff" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "staff" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "staff" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "emergency_request_log" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request_log" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "emergency_request_log" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request_log" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "campaign_donation_log" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation_log" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "campaign_donation_log" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation_log" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+
+    this.addSql(`alter table "blood_unit_actions" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit_actions" alter column "created_at" set default '2025-06-25T15:23:49.379Z';`);
+    this.addSql(`alter table "blood_unit_actions" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit_actions" alter column "updated_at" set default '2025-06-25T15:23:49.379Z';`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`alter table "account" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "account" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "account" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "account" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "admin" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "admin" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "admin" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "admin" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "campaign" drop column "blood_collection_date", drop column "metadata";`);
+
+    this.addSql(`alter table "campaign" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "campaign" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "campaign" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "campaign" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "customer" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "customer" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "customer" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "customer" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "campaign_donation" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "campaign_donation" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "blood_unit" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "blood_unit" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "emergency_request" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "emergency_request" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "hospital" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "hospital" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "hospital" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "hospital" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "staff" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "staff" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "staff" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "staff" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "emergency_request_log" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request_log" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "emergency_request_log" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "emergency_request_log" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "campaign_donation_log" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation_log" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "campaign_donation_log" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "campaign_donation_log" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+
+    this.addSql(`alter table "blood_unit_actions" alter column "created_at" type timestamptz using ("created_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit_actions" alter column "created_at" set default '2025-06-24T13:44:57.098Z';`);
+    this.addSql(`alter table "blood_unit_actions" alter column "updated_at" type timestamptz using ("updated_at"::timestamptz);`);
+    this.addSql(`alter table "blood_unit_actions" alter column "updated_at" set default '2025-06-24T13:44:57.099Z';`);
+  }
+
+}
