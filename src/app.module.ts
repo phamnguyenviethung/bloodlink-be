21fd641a82d1b5c952769c0f12fb3e1ce9901fe2
Viet Hung <viethung01234@gmail.com>
2025-07-25 19:12:22 +0700

    Refactor email configuration and update default sender address
    - Removed EMAIL_HOST and EMAIL_PORT from configuration validation.
    - Changed email transport service to 'gmail'.
    - Updated default sender address in sendEmail method to 'noreply@bloodlink.site'.

--- src/app.module.ts
+++ src/app.module.ts
@@ -63,10 +63,9 @@ import {
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().required(),
 
-        EMAIL_HOST: Joi.string().required(),
-        EMAIL_PORT: Joi.number().required(),
         EMAIL_USER: Joi.string().required(),
         EMAIL_PASS: Joi.string().required(),
+        EMAIL_FROM: Joi.string().required(),
 
         IMGUR_CLIENT_ID: Joi.string().required(),
 
