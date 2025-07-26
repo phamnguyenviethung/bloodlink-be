256abed02872ed1d0836d5ac002fd758c7137fe0
Viet Hung <viethung01234@gmail.com>
2025-07-26 07:59:10 +0700

    Update donation status mapping to use direct status values and add 'customer_checked_in' case

--- src/modules/donation/services/donation.service.ts
+++ src/modules/donation/services/donation.service.ts
@@ -222,6 +222,7 @@ export class DonationService {
 
     if (status) {
       where.currentStatus = status;
+      where.currentStatus = status;
     }
 
     if (campaignId) {
@@ -591,12 +592,12 @@ export class DonationService {
         return CampaignDonationStatus.APPOINTMENT_ABSENT;
       case 'customer_cancelled':
         return CampaignDonationStatus.CUSTOMER_CANCELLED;
+
       case 'customer_checked_in':
         return CampaignDonationStatus.CUSTOMER_CHECKED_IN;
-      case 'not_qualified':
-        return CampaignDonationStatus.NOT_QUALIFIED;
-      case 'no_show_after_checkin':
-        return CampaignDonationStatus.NO_SHOW_AFTER_CHECKIN;
+      case 'rejected':
+      case 'cancelled':
+
       default:
         return CampaignDonationStatus.APPOINTMENT_CONFIRMED;
     }
